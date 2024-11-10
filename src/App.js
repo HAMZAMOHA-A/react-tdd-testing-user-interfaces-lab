4a30f08171891e73f45efeb2d4d24715f4e39832
hamzamoha-a <hamzashukri29@gmail.com>
2024-11-10 10:31:46 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Hamza</h1>
+
+      {/* Image with alt text */}
+      <img
+        src="https://avatars.githubusercontent.com/u/180252737?v=4"
+        alt="Profile picture of [Your Name]"
+      />
+
+      {/* About Me section */}
+      <h2>About Me</h2>
+      <p>I'm a passionate developer who loves creating web applications!</p>
+
+      {/* Links to social media */}
+      <a href="https://github.com/[YourUsername]" target="_blank" rel="noopener noreferrer">
+        GitHub
+      </a>
+      <a href="https://linkedin.com/in/[YourUsername]" target="_blank" rel="noopener noreferrer">
+        LinkedIn
+      </a>
+    </div>
+  );
 }
 
 export default App;
